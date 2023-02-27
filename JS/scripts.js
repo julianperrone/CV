08fb47cfdec199d567a225bc593ddd2437358f9e
julianperrone <julianperrone555@live.com>
2023-02-27 18:43:38 -0300

    Script Working. Starting Mediaquerys

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,18 +1,19 @@
-function send(onclick=document.getElementById("send")) {
-    var nombre= document.getElementById("name").value;
-    var correo= document.getElementById("email").value;
-    var mensaje= document.getElementById("message").value;
-    document.getElementById("nameform").innerHTML = nombre
-    document.getElementById("emailform").innerHTML = correo
-    document.getElementById("msgform").innerHTML = mensaje
-}
-/*
-onclick=document.getElementById("send")
-    var nombre= document.getElementById("name").value;
-    var correo= document.getElementById("email").value;
-    var mensaje= document.getElementById("message").value;
-    document.getElementById("nameform").innerHTML = nombre
-    document.getElementById("emailform").innerHTML = correo
-    document.getElementById("msgform").innerHTML = mensaje
+const btn = document.getElementById("send")
 
+
+function printform() {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+    console.log(name)
+    console.log(email)
+    console.log(message)
+    alert("Se rellenaran los campos debajo del formulario con los datos ingresados")
+
+
+/*document.getElementById("send").addEventListener.onclick = function printform()
 */
+document.getElementById("nameform").innerHTML = name;
+document.getElementById("emailform").innerHTML = email;
+document.getElementById("msgform").innerHTML = message;
+}
